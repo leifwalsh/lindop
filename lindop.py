54079c637cb4b0e742f929cb862f38f12de706f7
Leif Walsh <leif.walsh@gmail.com>
2009-08-16 14:30:14 -0700

    that's what I get for not testing; also added C-c catching

--- lindop.py
+++ lindop.py
@@ -6,18 +6,18 @@ from __future__ import with_statement
 
 interface = 'wlan0'  # What interface to listen on?
 hosts = ['facebook.com', 'twitter.com']  # Hosts to discourage
-timedelay = 30.0  # How long must you abstain before you get the lights back?
+timedelay = 15.0  # How long must you abstain before you get the lights back?
 
 # What do we use to dim the screen? (xdim / acpi)
-dimmode = 'xdim'
-
-# xdim config
-xdim_lower = 0.5  # How much do we dim the lights?  (0-1)
+dimmode = 'acpi'
 
 # acpi config
 procbrightness  = '/proc/acpi/video/VID/LCD0/brightness'  # Where's the switch?
 brightnessdelta = 15  # How much do we dim the lights?  (percentage)
 minbrightness   = 10  # How low can you go?
+
+# xdim config
+xdim_lower = 0.7  # How much do we dim the lights?  (0-1)
 
 
 import os
@@ -27,10 +27,6 @@ from subprocess import call, Popen, PIPE
 import sys
 from threading import Event, Timer
 
-
-def xdim_brightness_functions():
-    return (lambda : call(['./xdim', str(xdim_lower)]),
-            lambda : call(['./xdim', '1']))
 
 def acpi_brightness_functions():
     with open(procbrightness, 'r') as br:
@@ -49,6 +45,10 @@ def acpi_brightness_functions():
             br.write('%d\n' % normalbrightness)
     return punish, unpunish
 
+def xdim_brightness_functions():
+    return (lambda : call(['./xdim', str(xdim_lower)]),
+            lambda : call(['./xdim', '1']))
+
 def capture_filter(hosts):
     return (' or '.join('(host %s)' % host.replace(':', ' and port ')
                         for host in hosts)).split()
@@ -58,10 +58,10 @@ def main(argv):
     args = ['tcpdump', '-i', interface, '-c', '1']
     args.extend(capture_filter(hosts))
 
-    if dimmode == 'xdim':
-        punish, unpunish = xdim_brightness_functions()
-    elif dimmode == 'acpi':
+    if dimmode == 'acpi':
         punish, unpunish = acpi_brightness_functions()
+    elif dimmode == 'xdim':
+        punish, unpunish = xdim_brightness_functions()
 
     killed = Event()
     def killer(fn):
@@ -75,20 +75,22 @@ def main(argv):
 
     with open('/dev/null', 'w') as null:
         while True:
-            tcpdump = Popen(args, stdout=null, stderr=PIPE)
+            try:
+                tcpdump = Popen(args, stdout=null, stderr=PIPE)
 
-            timer = Timer(timedelay, killer(tcpdump.terminate))
-            killed.clear()
-            timer.daemon = True
-            timer.start()
+                timer = Timer(timedelay, killer(tcpdump.terminate))
+                killed.clear()
+                timer.daemon = True
+                timer.start()
 
-            _, err = tcpdump.communicate()
+                _, err = tcpdump.communicate()
 
-            if not killed.is_set():
-                killed.set()
-                timer.cancel()
+                if not killed.is_set():
+                    killed.set()
+                    timer.cancel()
 
                 for line in err.split('\n'):
+                    mg = linere.match(line)
                     if mg and int(mg.group('packets')) != 0:
                         if not islower:
                             punish()
@@ -98,6 +100,10 @@ def main(argv):
                     if islower:
                         unpunish()
                     islower = False
+            except KeyboardInterrupt, e:
+                if islower:
+                    unpunish()
+                raise e
 
 
 if __name__ == '__main__':
